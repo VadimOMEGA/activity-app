e386307bc9043e08b833001f3724bd4ed7f77861
VadimOMEGA <stejaruldacilor2023@gmail.com>
2026-04-17 18:33:45 +0300

    removed eslint-ignore comment

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.useGlobalFilters(new GlobalExceptionFilter())
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	app.use(cookieParser())
 
 	app.enableCors({
